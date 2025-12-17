53e0f11987a5857923fd8c4fb50ece356b8a2067
Dayan Ruben <mail@dayanruben.com>
2025-12-17 10:54:04 -0300

    feat(build): migrate to android-kotlin multiplatform plugin
    Replaced the `com.android.library` plugin with the new `com.android.kotlin.multiplatform` plugin. This simplifies the build configuration by moving Android-specific settings into the `kotlin` block.

--- sssp/build.gradle.kts
+++ sssp/build.gradle.kts
@@ -1,9 +1,8 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.androidKmpLibrary)
     alias(libs.plugins.vanniktech.mavenPublish)
 }
 
@@ -12,9 +11,12 @@ version = libs.versions.sssp.get()
 
 kotlin {
     jvm()
-    androidTarget {
-        publishLibraryVariants("release")
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+
+    android {
+        namespace = "com.dayanruben.sssp"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
@@ -31,18 +33,6 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
-    }
-}
-
-android {
-    namespace = "com.dayanruben.sssp"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
